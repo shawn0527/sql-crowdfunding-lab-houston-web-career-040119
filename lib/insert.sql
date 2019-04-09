89e1a83112a1019741c5aac2495eb1df46f36e7c
Shawn Ren <shawnren527@gmail.com>
2019-04-09 03:17:15 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO projects(id, title, category, funding_goal, start_date, end_date)
+VALUES (1, 'project1', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(1, 'project1', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(2, 'project2', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(3, 'project3', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(4, 'project4', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(5, 'project5', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(6, 'project6', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(7, 'project7', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(8, 'project8', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(9, 'project9', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(10, 'project10', 'entertainment', 6000000, 'Aug92019', 'Aug82020');
