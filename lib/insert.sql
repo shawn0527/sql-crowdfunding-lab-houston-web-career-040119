5e02f6eb43aa398786a012e28303eff0e204517d
Shawn Ren <shawnren527@gmail.com>
2019-04-09 03:21:35 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,3 +10,9 @@ VALUES (1, 'project1', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
 (8, 'project8', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
 (9, 'project9', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
 (10, 'project10', 'entertainment', 6000000, 'Aug92019', 'Aug82020');
+
+
+INSERT INTO users(id, name, age)
+VALUES (1, 'a', 1), (2, 'b', 1),(3, 'c', 1), (4, 'd', 1),(5, 'e', 1), (6, 'f', 1),(7, 'g', 1), (8, 'h', 1),
+(9, 'i', 1), (10, 'j', 1),(11, 'k', 1), (12, 'l', 1),(13, 'm', 1), (14, 'n', 1),(15, 'o', 1), (16, 'p', 1),
+(17, 'q', 1), (18, 'r', 1),(19, 's', 1), (20, 't', 1);
