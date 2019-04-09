2043d74ed0700ae8d182bb8932a2e646ae57f22f
Shawn Ren <shawnren527@gmail.com>
2019-04-09 03:24:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,14 @@
 INSERT INTO projects(id, title, category, funding_goal, start_date, end_date)
 VALUES (1, 'project1', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(1, 'project1', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(2, 'project2', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(3, 'project3', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(4, 'project4', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(5, 'project5', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(6, 'project6', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(7, 'project7', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(8, 'project8', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(9, 'project9', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
-(10, 'project10', 'entertainment', 6000000, 'Aug92019', 'Aug82020');
+(2, 'project1', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(3, 'project2', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(4, 'project3', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(5, 'project4', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(6, 'project5', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(7, 'project6', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(8, 'project7', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(9, 'project8', 'entertainment', 6000000, 'Aug92019', 'Aug82020'),
+(10, 'project9', 'entertainment', 6000000, 'Aug92019', 'Aug82020');
 
 
 INSERT INTO users(id, name, age)
