069d1091b4dc6c43128f0ead7021bb11e6869c91
Shawn Ren <shawnren527@gmail.com>
2019-04-09 03:04:11 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,2 @@
-  CREATE TABLE projects(id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+CREATE TABLE projects(id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date TEXT, end_date TEXT);
+
