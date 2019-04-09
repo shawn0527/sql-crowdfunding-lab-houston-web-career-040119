5c9c6ad580468ce7f8e79b64773bf4de4d96e1c7
Shawn Ren <shawnren527@gmail.com>
2019-04-09 03:54:59 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -37,7 +37,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT users.name, SUM(pledges.amount)
 FROM users
 INNER JOIN pledges
-ON users.id = projects.user_id
+ON users.id = pledges.user_id
 GROUP BY users.name
 ORDER BY pledges.amount, users.name"
 end
